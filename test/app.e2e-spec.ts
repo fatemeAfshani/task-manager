3a0852b4996e4168cd844e70066aafac02b9612e
f.afshani <fatemeh.afshani78@gmail.com>
2023-06-29 00:44:49 +0330

    test: complete configuration and add successful signup test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
+import * as pactum from 'pactum';
 import { AppModule } from './../src/app.module';
+import { DbProvider } from '../src/utils/db/db.provider';
+import * as config from 'config';
+import { UserCredentialsDto } from 'src/users/dto/user-credentials.dto';
 
 describe('App (e2e)', () => {
   let app: INestApplication;
-
+  let dbProvider: DbProvider;
+  const port = config.get('app.port');
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -15,13 +19,28 @@ describe('App (e2e)', () => {
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
     await app.init();
+    app.listen(port);
+    dbProvider = app.get(DbProvider);
+    await dbProvider.clearDB();
   });
 
   afterAll(async () => {
     await app.close();
   });
 
-  it('some randome', () => {
-    expect(2 + 2).toBe(4);
+  describe('user', () => {
+    describe('signup', () => {
+      const userDto: UserCredentialsDto = {
+        username: 'fateme',
+        password: 'Some123@',
+      };
+      it('successful signup', () => {
+        return pactum
+          .spec()
+          .post(`http://localhost:${port}/users/signup`)
+          .withBody(userDto)
+          .expectStatus(201);
+      });
+    });
   });
 });
